1571cdda82320e7afd2a363faeff590d7f9153fe
Marius Du Plooy <woestyn101@gmail.com>
2023-12-09 11:23:23 -0500

    all time buttons and localstorage added

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,14 @@ $(function () {
        var btn08 = $("#btn08");
        var btn09 = $("#btn09");
        var btn10 = $("#btn10");
+       var btn11 = $("#btn11");
+       var btn12 = $("#btn12");
+       var btn1 = $("#btn1");
+       var btn2 = $("#btn2");
+       var btn3 = $("#btn3");
+       var btn4 = $("#btn4");
+       var btn5 = $("#btn5");
+       var btn6pm = $("#btn6pm");
        
        //input vars
       var input06 = $("#input06");
@@ -18,6 +26,14 @@ $(function () {
      var input08 = $("#input08");
      var input09 = $("#input09");
      var input10 = $("#input10");
+     var input11 = $("#input11");
+     var input12 = $("#input12");
+     var input1 = $("#input1");
+     var input2 = $("#input2");
+     var input3 = $("#input3");
+     var input4 = $("#input4");
+     var input5 = $("#input5");
+     var input6pm = $("#input6pm");
   
       
      
@@ -41,6 +57,38 @@ $(function () {
          var getInput10 = localStorage.getItem("event10am");
          console.log("local storage:", getInput10);
            input10.text(getInput10);
+
+         var getInput11 = localStorage.getItem("event11am");
+         console.log("local storage:", getInput11);
+           input11.text(getInput11);
+
+         var getInput12 = localStorage.getItem("event12pm");
+         console.log("local storage:", getInput12);
+           input12.text(getInput12);
+
+         var getInput1 = localStorage.getItem("event1pm");
+         console.log("local storage:", getInput1);
+           input1.text(getInput1);
+
+         var getInput2 = localStorage.getItem("event2pm");
+         console.log("local storage:", getInput2);
+           input2.text(getInput2);
+
+         var getInput3 = localStorage.getItem("event3pm");
+         console.log("local storage:", getInput3);
+           input3.text(getInput3);
+
+         var getInput4 = localStorage.getItem("event4pm");
+         console.log("local storage:", getInput4);
+           input4.text(getInput4);
+
+         var getInput5 = localStorage.getItem("event5pm");
+         console.log("local storage:", getInput5);
+           input5.text(getInput5);
+
+         var getInput6pm = localStorage.getItem("event6pm");
+         console.log("local storage:", getInput6pm);
+           input6pm.text(getInput6pm);
   
     
 
@@ -83,6 +131,72 @@ $(function () {
          console.log("Name:", input10.val());
          localStorage.setItem("event10am",input10.val())
          };
+
+          // 11 am button
+          btn11.on('click', store11);
+
+         function store11(){
+         console.log("Name:", input11.val());
+         localStorage.setItem("event11am",input11.val())
+         };
+
+          // 12 pm button
+          btn12.on('click', store12);
+
+         function store12(){
+         console.log("Name:", input12.val());
+         localStorage.setItem("event12pm",input12.val())
+         };
+
+          // 1 pm button
+          btn1.on('click', store1);
+
+         function store1(){
+         console.log("Name:", input1.val());
+         localStorage.setItem("event1pm",input1.val())
+         };
+
+          // 2 pm button
+          btn2.on('click', store2);
+
+         function store2(){
+         console.log("Name:", input2.val());
+         localStorage.setItem("event2pm",input2.val())
+         };
+
+          // 3 pm button
+          btn3.on('click', store3);
+
+         function store3(){
+         console.log("Name:", input3.val());
+         localStorage.setItem("event3pm",input3.val())
+         };
+
+          // 4 pm button
+          btn4.on('click', store4);
+
+         function store4(){
+         console.log("Name:", input4.val());
+         localStorage.setItem("event4pm",input4.val())
+         };
+
+          // 5 pm button
+          btn5.on('click', store5);
+
+         function store5(){
+         console.log("Name:", input5.val());
+         localStorage.setItem("event5pm",input5.val())
+         };
+
+          // 6 pm button
+          btn6pm.on('click', store6pm);
+
+         function store6pm(){
+         console.log("Name:", input6pm.val());
+         localStorage.setItem("event6pm",input6pm.val())
+         };
+
+
                     
   // setting background color of inputfield
     $("#input06").css("background", "#d3d3d3");
